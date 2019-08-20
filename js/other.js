995e61a74fb2b2296f0243753088878a94cab81a
Kyle <doors.du@gmail.com>
2019-08-20 18:02:09 +0800

    initialize

---
+++ js/other.js
@@ -0,0 +1,7 @@
+console.log("Starting replace other words...")
+/*
+ * 不定期持续更新
+ * 2019.08.20 by Kyle
+ * Walkover -> 退赛
+ */
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Walkover", "g"), "退赛");
---
+++ js/players-ms.js
@@ -0,0 +1,37 @@
+console.log("Starting replace ms players...")
+/*
+ * 不定期持续更新
+ * 2019.08.20 by Kyle
+ * LIN Dan -> 林丹
+ * Kento MOMOTA -> 桃田贤斗
+ * CHOU Tien Chen -> 周天成
+ * CHEN Long -> 谌龙
+ * Jonatan CHRISTIE -> 乔纳坦
+ * Anders ANTONSEN -> 安东森
+ * Anthony Sinisuka GINTING -> 金廷
+ * KIDAMBI Srikanth -> 斯里坎特
+ * Kenta NISHIMOTO -> 西本拳太
+ * NG Ka Long Angus -> 伍家朗
+ * Sameer VERMA -> 维尔马
+ * Kantaphon WANGCHAROEN -> 坎塔蓬·旺差伦
+ * Kanta TSUNEYAMA -> 常山干太
+ * LEE Zii Jia -> 李梓嘉
+ * Tommy SUGIARTO -> 苏吉亚托
+ * SAI PRANEETH B. -> 塞·帕拉内斯
+ */
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("LIN Dan", "g"), "林丹");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Kento MOMOTA", "g"), "桃田贤斗");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("CHOU Tien Chen", "g"), "周天成");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("CHEN Long", "g"), "谌龙");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Jonatan CHRISTIE", "g"), "乔纳坦");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Anders ANTONSEN", "g"), "安东森");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Anthony Sinisuka GINTING", "g"), "金廷");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("KIDAMBI Srikanth", "g"), "斯里坎特");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Kenta NISHIMOTO", "g"), "西本拳太");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("NG Ka Long Angus", "g"), "伍家朗");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Sameer VERMA", "g"), "维尔马");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Kantaphon WANGCHAROEN", "g"), "坎塔蓬·旺差伦");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Kanta TSUNEYAMA", "g"), "常山干太");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("LEE Zii Jia", "g"), "李梓嘉");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Tommy SUGIARTO", "g"), "苏吉亚托");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("SAI PRANEETH B.", "g"), "塞·帕拉内斯");
